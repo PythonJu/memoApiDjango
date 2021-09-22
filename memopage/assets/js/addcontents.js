d6a1567786296e8942cd2e9d985c8da01de1e8bc
PythonJu <d87496789@gmail.com>
2021-09-22 10:20:34 +0900

    コンテンツ追加ボタンを調整

---
+++ memopage/assets/js/addcontents.js
@@ -0,0 +1,22 @@
+// document.getElementById('create_toggle_button').addEventListener('click', function() {
+//     const form = document.getElementById('create_form');
+//     if (form.style.display === 'none') {
+//         form.style.display = 'block';
+//     } else {
+//         form.style.display = 'none';
+//     }
+// });
+
+
+document.getElementById('create_toggle_button').addEventListener('click', () => {
+    showToggle();
+});
+
+function showToggle(){
+    const form = document.getElementById('create_form');
+    if(form.style.display === 'none'){
+        form.style.display = 'block';
+    }else{
+        form.style.display = 'none';
+    }
+}
